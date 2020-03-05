ea27d423a90682ab923e240b1e1f56d8123df385
JHZ21 <1109050900@qq.com>
2020-03-06 00:07:11 +0800

    finsh getArticleUrl of learnContent and so on

--- src/api/learn.ts
+++ src/api/learn.ts
@@ -1,28 +1,16 @@
 import axios from "axios"
 
+export const getLearnContent = (data: any) =>
+  axios({
+    method: "POST",
+    url: `${process.env.VUE_APP_BASE_API}/learn/getContent`,
+    data
+  })
+
 export const getLearnRotationUrl = () =>
   axios({
     method: "GET",
     url: `${process.env.VUE_APP_BASE_API}/learn/rotationUrl`
-  })
-
-export const getLearnCard = () =>
-  axios({
-    method: "get",
-    url: "learn/card"
-  })
-
-export const getOContentUrl = () =>
-  axios({
-    method: "POST",
-    url: "learn/getocententurl"
-  })
-
-export const getContent = (data: any) =>
-  axios({
-    method: "POST",
-    url: "/learn/getContent",
-    data
   })
 
 export const getLearnNavData = () => {
