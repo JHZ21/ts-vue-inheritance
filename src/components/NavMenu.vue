139a9127a96b9bc3587ffff956e3d78899e3d3dc
JHZ21 <jhz21@qq.com>
2021-03-06 11:36:31 +0800

    fix: 添加cert文件，navData数据demo

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -22,7 +22,7 @@
 <script lang="ts">
 import { Vue, Component, Prop, Watch, Emit } from "vue-property-decorator"
 import { NavRow, ASelectedType } from "@/utils/interface.ts"
-
+// LearnNavData
 const data1: any = {
   index: 0,
   label: "方向:",
@@ -30,8 +30,8 @@ const data1: any = {
     { id: "fontEnd", text: "前端" },
     { id: "backEnd", text: "后端" },
     { id: "dataBase", text: "数据库" },
-    { id: "principlesOfComputer", text: "计算机原理" }
-  ]
+    { id: "principlesOfComputer", text: "计算机原理" },
+  ],
 }
 const data2: any = {
   index: 1,
@@ -41,12 +41,12 @@ const data2: any = {
       { id: "HTML/CSS", text: "HTML/CSS" },
       { id: "JavaScript", text: "JavaScript" },
       { id: "Vue.js", text: "Vue.js" },
-      { id: "Sass/Less", text: "Sass/Less" }
+      { id: "Sass/Less", text: "Sass/Less" },
     ],
     backEnd: [{ id: "a", text: "a" }],
     dataBase: [{ id: "a", text: "a" }],
-    principlesOfComputer: [{ id: "a", text: "a" }]
-  }
+    principlesOfComputer: [{ id: "a", text: "a" }],
+  },
 }
 const data3: any = {
   index: 2,
@@ -54,12 +54,275 @@ const data3: any = {
   aData: [
     { id: "junior", text: "初级" },
     { id: "intermediate", text: "中级" },
-    { id: "senior", text: "高级" }
-  ]
+    { id: "senior", text: "高级" },
+  ],
+}
+// competNavData
+const competNavData1 = {
+  label: "比赛:",
+  aData: [
+    {
+      id: "dmtds.zjnu.edu.cn",
+      text: "浙江省大学生多媒体作品设计竞赛",
+    },
+    {
+      id: "jsjds.ruc.edu.cn",
+      text: "中国大学生计算机设计大赛",
+    },
+    {
+      id: "cy.ncss.cn",
+      text: "中国'互联网+'大学生创新创业大赛",
+    },
+    {
+      id: "gjcxcy.bjtu.edu.cn",
+      text: "国创",
+    },
+    {
+      id: "hsqn.zust.edu.cn",
+      text: "新苗",
+    },
+    {
+      id: "chunmeng",
+      text: "春萌",
+    },
+  ],
+}
+const competNavData2 = {
+  label: "时间:",
+  aData: {
+    "dmtds.zjnu.edu.cn": [
+      {
+        id: "2021.1",
+        text: "2021上",
+      },
+      {
+        id: "2020.7",
+        text: "2020下",
+      },
+      {
+        id: "2020.1",
+        text: "2020上",
+      },
+      {
+        id: "2019.7",
+        text: "2019下",
+      },
+      {
+        id: "2019.1",
+        text: "2019上",
+      },
+      {
+        id: "2018.7",
+        text: "2018下",
+      },
+      {
+        id: "2018.1",
+        text: "2018上",
+      },
+      {
+        id: "2017.7",
+        text: "2017下",
+      },
+      {
+        id: "2017.1",
+        text: "2017上",
+      },
+    ],
+    "jsjds.ruc.edu.cn": [
+      {
+        id: "2021.1",
+        text: "2021上",
+      },
+      {
+        id: "2020.7",
+        text: "2020下",
+      },
+      {
+        id: "2020.1",
+        text: "2020上",
+      },
+      {
+        id: "2019.7",
+        text: "2019下",
+      },
+      {
+        id: "2019.1",
+        text: "2019上",
+      },
+      {
+        id: "2018.7",
+        text: "2018下",
+      },
+      {
+        id: "2018.1",
+        text: "2018上",
+      },
+      {
+        id: "2017.7",
+        text: "2017下",
+      },
+      {
+        id: "2017.1",
+        text: "2017上",
+      },
+    ],
+    "cy.ncss.cn": [
+      {
+        id: "2021.1",
+        text: "2021上",
+      },
+      {
+        id: "2020.7",
+        text: "2020下",
+      },
+      {
+        id: "2020.1",
+        text: "2020上",
+      },
+      {
+        id: "2019.7",
+        text: "2019下",
+      },
+      {
+        id: "2019.1",
+        text: "2019上",
+      },
+      {
+        id: "2018.7",
+        text: "2018下",
+      },
+      {
+        id: "2018.1",
+        text: "2018上",
+      },
+      {
+        id: "2017.7",
+        text: "2017下",
+      },
+      {
+        id: "2017.1",
+        text: "2017上",
+      },
+    ],
+    "gjcxcy.bjtu.edu.cn": [
+      {
+        id: "2021.1",
+        text: "2021上",
+      },
+      {
+        id: "2020.7",
+        text: "2020下",
+      },
+      {
+        id: "2020.1",
+        text: "2020上",
+      },
+      {
+        id: "2019.7",
+        text: "2019下",
+      },
+      {
+        id: "2019.1",
+        text: "2019上",
+      },
+      {
+        id: "2018.7",
+        text: "2018下",
+      },
+      {
+        id: "2018.1",
+        text: "2018上",
+      },
+      {
+        id: "2017.7",
+        text: "2017下",
+      },
+      {
+        id: "2017.1",
+        text: "2017上",
+      },
+    ],
+    "hsqn.zust.edu.cn": [
+      {
+        id: "2021.1",
+        text: "2021上",
+      },
+      {
+        id: "2020.7",
+        text: "2020下",
+      },
+      {
+        id: "2020.1",
+        text: "2020上",
+      },
+      {
+        id: "2019.7",
+        text: "2019下",
+      },
+      {
+        id: "2019.1",
+        text: "2019上",
+      },
+      {
+        id: "2018.7",
+        text: "2018下",
+      },
+      {
+        id: "2018.1",
+        text: "2018上",
+      },
+      {
+        id: "2017.7",
+        text: "2017下",
+      },
+      {
+        id: "2017.1",
+        text: "2017上",
+      },
+    ],
+    chunmeng: [
+      {
+        id: "2021.1",
+        text: "2021上",
+      },
+      {
+        id: "2020.7",
+        text: "2020下",
+      },
+      {
+        id: "2020.1",
+        text: "2020上",
+      },
+      {
+        id: "2019.7",
+        text: "2019下",
+      },
+      {
+        id: "2019.1",
+        text: "2019上",
+      },
+      {
+        id: "2018.7",
+        text: "2018下",
+      },
+      {
+        id: "2018.1",
+        text: "2018上",
+      },
+      {
+        id: "2017.7",
+        text: "2017下",
+      },
+      {
+        id: "2017.1",
+        text: "2017上",
+      },
+    ],
+  },
 }
 
 @Component({
-  name: "NavMenu"
+  name: "NavMenu",
 })
 export default class extends Vue {
   @Prop({ required: true }) navData!: NavRow[]
